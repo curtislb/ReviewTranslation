0d6fd0116f9ee36f15ce8fab267d2ab937b45939
Curtis Belmonte <curtislb@princeton.edu>
2016-04-16 19:37:57 -0400

    Test loading into pandas dataframe

--- process_reviews.py
+++ process_reviews.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python
 
+import gzip
+import pandas as pd
+import sys
+
+
+def parse_reviews(path):
+    g = gzip.open(path, 'rb')
+    for l in g:
+        yield eval(l)
+
+
+def get_data_frame(path):
+    i = 0
+    df = {}
+    for d in parse_reviews(path):
+        df[i] = d
+        i += 1
+
+    return pd.DataFrame.from_dict(df, orient='index')
+
+
+def main():
+    df = get_data_frame(sys.argv[1])
+    print df.size
+
+
+if __name__ == '__main__':
+    main()
